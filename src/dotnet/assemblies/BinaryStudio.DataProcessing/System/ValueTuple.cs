2867efbae181da4b6dab82ea4ff0460c63bfcbb6
maystrenco <maystrenco@ro.ru>
2021-11-25 12:24:11 +0300

    netcoreapp3.1

--- src/dotnet/assemblies/BinaryStudio.DataProcessing/System/ValueTuple.cs
+++ src/dotnet/assemblies/BinaryStudio.DataProcessing/System/ValueTuple.cs
@@ -1,4 +1,4 @@
-#if !NET_4_7
+#if NET45
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
