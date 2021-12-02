4a445c88d0038d5277d41a47ee0e76cc3b762550
maystrenco <maystrenco@ro.ru>
2021-12-02 09:44:42 +0300

    {comments}

--- src/dotnet/assemblies/BinaryStudio.Security.Cryptography.AbstractSyntaxNotation/Asn1UniversalObject.cs
+++ src/dotnet/assemblies/BinaryStudio.Security.Cryptography.AbstractSyntaxNotation/Asn1UniversalObject.cs
@@ -4,13 +4,159 @@ using BinaryStudio.IO;
 
 namespace BinaryStudio.Security.Cryptography.AbstractSyntaxNotation
     {
+    /// <summary>
+    /// </summary>
+    /// <remarks>
+    /// Represents a base class for types:
+    /// <table style="font-family: Consolas;width:100%;border-collapse:collapSe;border:none;mso-border-alt:solid windowtext .5pt;mso-yfti-tbllook:1184;mso-padding-alt:0cm 5.4pt 0cm 5.4pt; background-color: white;">
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="BIT STRING"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1BitString"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="BOOLEAN"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1Boolean"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="EMBEDDED PDV"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1EmbeddedPDV"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="EOC"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1EndOfContent"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="ENUMERATED"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1Enum"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="EXTERNAL"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1External"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="INTEGER"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1Integer"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="NULL"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1Null"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="OBJECT DESCRIPTOR"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1ObjectDescriptor"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="OBJECT IDENTIFIER"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1ObjectIdentifier"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="OCTET STRING"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1OctetString"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="REAL"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1Real"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="SEQUENCE"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1Sequence"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see langword="SET"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1Set"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 0pt 0cm 5.4pt;">
+    ///         <see langword="GENERALSTRING"/><br/>
+    ///         <see langword="GRAPHICSTRING"/><br/>
+    ///         <see langword="IA5STRING"/><br/>
+    ///         <see langword="NUMERICSTRING"/><br/>
+    ///         <see langword="PRINTABLESTRING"/><br/>
+    ///         <see langword="T61STRING"/><br/>
+    ///         <see langword="BMPSTRING"/><br/>
+    ///         <see langword="UNIVERSALSTRING"/><br/>
+    ///         <see langword="UTF8STRING"/><br/>
+    ///         <see langword="VIDEOTEXSTRING"/><br/>
+    ///         <see langword="VISIBLESTRING"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1String"/>
+    ///     </td>
+    ///   </tr>
+    ///   <tr>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 0pt 0cm 5.4pt;">
+    ///       <see langword="UTCTIME"/><br/>
+    ///       <see langword="GENERALIZEDTIME"/>
+    ///     </td>
+    ///     <td style="border:solid windowtext 1.0pt;mso-border-alt:solid windowtext .5pt;padding:0cm 5.4pt 0cm 5.4pt;">
+    ///       <see cref="Asn1Time"/>
+    ///     </td>
+    ///   </tr>
+    /// </table>
+    /// </remarks>
     public abstract class Asn1UniversalObject : Asn1Object, IAsn1Object
         {
         [DebuggerBrowsable(DebuggerBrowsableState.Never)] public override Asn1ObjectClass Class { get { return Asn1ObjectClass.Universal; }}
         [DebuggerBrowsable(DebuggerBrowsableState.Never)] SByte IAsn1Object.Type { get { return (SByte)Type; }}
         public abstract Asn1ObjectType Type { get; }
 
-        protected Asn1UniversalObject(ReadOnlyMappingStream source, Int64 forceoffset)
+        internal Asn1UniversalObject(ReadOnlyMappingStream source, Int64 forceoffset)
             : base(source, forceoffset)
             {
             }
