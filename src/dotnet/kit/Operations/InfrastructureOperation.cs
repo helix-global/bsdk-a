2a84bcb337695ae2ae71460a7e1d739729a72641
maistrenko <maistrenko@fintech.ru>
2021-12-10 16:33:53 +0300

    {kit}

--- src/dotnet/kit/Operations/InfrastructureOperation.cs
+++ src/dotnet/kit/Operations/InfrastructureOperation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using BinaryStudio.Security.Cryptography.CryptographyServiceProvider;
 using Options;
@@ -17,10 +18,17 @@ namespace Operations
             output.WriteLine("AvailableProviders:");
             foreach (var type in CryptographicContext.AvailableProviders) {
                 output.WriteLine($"  {type.Key}:{type.Value}");
-                using (var context = new CryptographicContext(null, type.Key, type.Value, CryptographicContextFlags.CRYPT_SILENT|CryptographicContextFlags.CRYPT_VERIFYCONTEXT, null)) {
-                    foreach (var algid in context.SupportedAlgorithms) {
-                        output.WriteLine($"    {algid.Key}:{algid.Value}");
+                try
+                    {
+                    using (var context = new CryptographicContext(null, type.Key, type.Value, CryptographicContextFlags.CRYPT_SILENT|CryptographicContextFlags.CRYPT_VERIFYCONTEXT, null)) {
+                        foreach (var algid in context.SupportedAlgorithms) {
+                            output.WriteLine($"    {algid.Key}:{algid.Value}");
+                            }
                         }
+                    }
+                catch (Exception e)
+                    {
+                    Console.WriteLine(e);
                     }
                 }
             }
