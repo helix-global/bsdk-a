91a4d3b838dd9f3a3582cc2fc172d782a634b9b7
maystrenco <maystrenco@ro.ru>
2021-12-14 22:09:27 +0300

    {openssl-sha1}

--- src/dotnet/assemblies/BinaryStudio.Security.Cryptography.CryptographyServiceProvider/CryptographyServiceProvider/Internal/OpenSSL/IOpenSSLLibrary.cs
+++ src/dotnet/assemblies/BinaryStudio.Security.Cryptography.CryptographyServiceProvider/CryptographyServiceProvider/Internal/OpenSSL/IOpenSSLLibrary.cs
@@ -5,14 +5,17 @@ namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
     {
     internal interface IOpenSSLLibrary
         {
+        Int32 SHA1_Init(ref SHA_CTX c);
         Int32 SHA256_Init(ref SHA256_CTX c);
         Int32 SHA224_Init(ref SHA256_CTX c);
         Int32 SHA384_Init(ref SHA512_CTX c);
         Int32 SHA512_Init(ref SHA512_CTX c);
+        Int32 SHA1_Final([MarshalAs(UnmanagedType.LPArray)]Byte[] digest, ref SHA_CTX c);
         Int32 SHA256_Final([MarshalAs(UnmanagedType.LPArray)]Byte[] digest, ref SHA256_CTX c);
         Int32 SHA224_Final([MarshalAs(UnmanagedType.LPArray)]Byte[] digest, ref SHA256_CTX c);
         Int32 SHA384_Final([MarshalAs(UnmanagedType.LPArray)]Byte[] digest, ref SHA512_CTX c);
         Int32 SHA512_Final([MarshalAs(UnmanagedType.LPArray)]Byte[] digest, ref SHA512_CTX c);
+        unsafe Int32 SHA1_Update(ref SHA_CTX c, Byte* data, IntPtr length);
         unsafe Int32 SHA256_Update(ref SHA256_CTX c, Byte* data, IntPtr length);
         unsafe Int32 SHA224_Update(ref SHA256_CTX c, Byte* data, IntPtr length);
         unsafe Int32 SHA384_Update(ref SHA512_CTX c, Byte* data, IntPtr length);
--- src/dotnet/assemblies/BinaryStudio.Security.Cryptography.CryptographyServiceProvider/CryptographyServiceProvider/Internal/OpenSSL/OpenSSLLibrary.cs
+++ src/dotnet/assemblies/BinaryStudio.Security.Cryptography.CryptographyServiceProvider/CryptographyServiceProvider/Internal/OpenSSL/OpenSSLLibrary.cs
@@ -10,6 +10,11 @@ namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
             {
             }
 
+        public Int32 SHA1_Init(ref SHA_CTX c)
+            {
+            return EnsureProcedure("SHA1_Init", ref FSHA1_Init)(ref c);
+            }
+
         Int32 IOpenSSLLibrary.SHA256_Init(ref SHA256_CTX c)
             {
             return EnsureProcedure("SHA256_Init", ref FSHA256_Init)(ref c);
@@ -20,6 +25,11 @@ namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
             return EnsureProcedure("SHA512_Init", ref FSHA512_Init)(ref c);
             }
 
+        public Int32 SHA1_Final(Byte[] digest, ref SHA_CTX c)
+            {
+            return EnsureProcedure("SHA1_Final", ref FSHA1_Final)(digest, ref c);
+            }
+
         Int32 IOpenSSLLibrary.SHA256_Final(Byte[] digest, ref SHA256_CTX c)
             {
             return EnsureProcedure("SHA256_Final", ref FSHA256_Final)(digest, ref c);
@@ -28,6 +38,11 @@ namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
         public Int32 SHA512_Final(Byte[] digest, ref SHA512_CTX c)
             {
             return EnsureProcedure("SHA512_Final", ref FSHA512_Final)(digest, ref c);
+            }
+
+        public unsafe Int32 SHA1_Update(ref SHA_CTX c, Byte* data, IntPtr length)
+            {
+            return EnsureProcedure("SHA1_Update", ref FSHA1_Update)(ref c, data, length);
             }
 
         public unsafe Int32 SHA256_Update(ref SHA256_CTX c, Byte* data, IntPtr length)
@@ -70,21 +85,27 @@ namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
             return EnsureProcedure("SHA512_Update", ref FSHA512_Update)(ref c, data, length);
             }
 
+        private delegate Int32 DSHA1_Init(ref SHA_CTX c);
         private delegate Int32 DSHA256_Init(ref SHA256_CTX c);
         private delegate Int32 DSHA512_Init(ref SHA512_CTX c);
+        private unsafe delegate Int32 DSHA1_Update(ref SHA_CTX c, Byte* data, IntPtr length);
         private unsafe delegate Int32 DSHA256_Update(ref SHA256_CTX c, Byte* data, IntPtr length);
         private unsafe delegate Int32 DSHA512_Update(ref SHA512_CTX c, Byte* data, IntPtr length);
+        private delegate Int32 DSHA1_Final(Byte[] digest, ref SHA_CTX c);
         private delegate Int32 DSHA256_Final(Byte[] digest, ref SHA256_CTX c);
         private delegate Int32 DSHA512_Final(Byte[] digest, ref SHA512_CTX c);
 
+        private DSHA1_Init     FSHA1_Init;
         private DSHA256_Init   FSHA256_Init;
         private DSHA256_Init   FSHA224_Init;
         private DSHA512_Init   FSHA384_Init;
         private DSHA512_Init   FSHA512_Init;
+        private DSHA1_Update   FSHA1_Update;
         private DSHA256_Update FSHA256_Update;
         private DSHA256_Update FSHA224_Update;
         private DSHA512_Update FSHA384_Update;
         private DSHA512_Update FSHA512_Update;
+        private DSHA1_Final    FSHA1_Final;
         private DSHA256_Final  FSHA256_Final;
         private DSHA256_Final  FSHA224_Final;
         private DSHA512_Final  FSHA384_Final;
