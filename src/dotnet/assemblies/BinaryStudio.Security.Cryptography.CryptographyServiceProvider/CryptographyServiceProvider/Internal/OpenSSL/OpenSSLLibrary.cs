0f2d8e9c5e3a618eadf26258d492a7255c83e1c9
maystrenco <maystrenco@ro.ru>
2021-12-14 21:56:02 +0300

    {openssl-sha384-sha512}

--- src/dotnet/assemblies/BinaryStudio.Security.Cryptography.CryptographyServiceProvider/CryptographyServiceProvider/Internal/OpenSSL/OpenSSLLibrary.cs
+++ src/dotnet/assemblies/BinaryStudio.Security.Cryptography.CryptographyServiceProvider/CryptographyServiceProvider/Internal/OpenSSL/OpenSSLLibrary.cs
@@ -15,9 +15,19 @@ namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
             return EnsureProcedure("SHA256_Init", ref FSHA256_Init)(ref c);
             }
 
+        public Int32 SHA512_Init(ref SHA512_CTX c)
+            {
+            return EnsureProcedure("SHA512_Init", ref FSHA512_Init)(ref c);
+            }
+
         Int32 IOpenSSLLibrary.SHA256_Final(Byte[] digest, ref SHA256_CTX c)
             {
             return EnsureProcedure("SHA256_Final", ref FSHA256_Final)(digest, ref c);
+            }
+
+        public Int32 SHA512_Final(Byte[] digest, ref SHA512_CTX c)
+            {
+            return EnsureProcedure("SHA512_Final", ref FSHA512_Final)(digest, ref c);
             }
 
         public unsafe Int32 SHA256_Update(ref SHA256_CTX c, Byte* data, IntPtr length)
@@ -30,9 +40,19 @@ namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
             return EnsureProcedure("SHA224_Init", ref FSHA224_Init)(ref c);
             }
 
+        public Int32 SHA384_Init(ref SHA512_CTX c)
+            {
+            return EnsureProcedure("SHA384_Init", ref FSHA384_Init)(ref c);
+            }
+
         Int32 IOpenSSLLibrary.SHA224_Final(Byte[] digest, ref SHA256_CTX c)
             {
             return EnsureProcedure("SHA224_Final", ref FSHA224_Final)(digest, ref c);
+            }
+
+        public Int32 SHA384_Final(Byte[] digest, ref SHA512_CTX c)
+            {
+            return EnsureProcedure("SHA384_Final", ref FSHA384_Final)(digest, ref c);
             }
 
         public unsafe Int32 SHA224_Update(ref SHA256_CTX c, Byte* data, IntPtr length)
@@ -40,14 +60,34 @@ namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
             return EnsureProcedure("SHA224_Update", ref FSHA224_Update)(ref c, data, length);
             }
 
+        public unsafe Int32 SHA384_Update(ref SHA512_CTX c, Byte* data, IntPtr length)
+            {
+            return EnsureProcedure("SHA384_Update", ref FSHA384_Update)(ref c, data, length);
+            }
+
+        public unsafe Int32 SHA512_Update(ref SHA512_CTX c, Byte* data, IntPtr length)
+            {
+            return EnsureProcedure("SHA512_Update", ref FSHA512_Update)(ref c, data, length);
+            }
+
         private delegate Int32 DSHA256_Init(ref SHA256_CTX c);
+        private delegate Int32 DSHA512_Init(ref SHA512_CTX c);
         private unsafe delegate Int32 DSHA256_Update(ref SHA256_CTX c, Byte* data, IntPtr length);
+        private unsafe delegate Int32 DSHA512_Update(ref SHA512_CTX c, Byte* data, IntPtr length);
         private delegate Int32 DSHA256_Final(Byte[] digest, ref SHA256_CTX c);
+        private delegate Int32 DSHA512_Final(Byte[] digest, ref SHA512_CTX c);
+
         private DSHA256_Init   FSHA256_Init;
-        private DSHA256_Update FSHA256_Update;
-        private DSHA256_Final  FSHA256_Final;
         private DSHA256_Init   FSHA224_Init;
+        private DSHA512_Init   FSHA384_Init;
+        private DSHA512_Init   FSHA512_Init;
+        private DSHA256_Update FSHA256_Update;
         private DSHA256_Update FSHA224_Update;
+        private DSHA512_Update FSHA384_Update;
+        private DSHA512_Update FSHA512_Update;
+        private DSHA256_Final  FSHA256_Final;
         private DSHA256_Final  FSHA224_Final;
+        private DSHA512_Final  FSHA384_Final;
+        private DSHA512_Final  FSHA512_Final;
         }
     }
---
+++ src/dotnet/assemblies/BinaryStudio.Security.Cryptography.CryptographyServiceProvider/CryptographyServiceProvider/Internal/OpenSSL/SHA512_CTX.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BinaryStudio.Security.Cryptography.CryptographyServiceProvider
+    {
+    using SHA_LONG=UInt32;
+    using SHA_LONG64=UInt64;
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct SHA512_CTX
+        {
+        private const Int32 SHA_LBLOCK = 16;
+        public unsafe fixed SHA_LONG64 h[8];
+        public readonly SHA_LONG64 Nl, Nh;
+        public unsafe fixed SHA_LONG64 data[SHA_LBLOCK];
+        public readonly SHA_LONG num, md_len;
+        }
+    }
